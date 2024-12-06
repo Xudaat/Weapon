56a11a046b53421b5a8bd6a90da0661e4a205463
unknown <xudaatm@gmail.com>
2024-12-06 21:17:21 +0400

    added task

---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Weapon
+{
+   internal class Program
+   {
+        static void Main(string[] args)
+        {
+           
+            Weapon myWeapon = new Weapon(30, 10, "Single");
+
+            
+            while (true)
+            {
+                Console.WriteLine("\nQısayollar:");
+                Console.WriteLine("0 - İnformasiya almaq");
+                Console.WriteLine("1 - Shoot metodu ucun");
+                Console.WriteLine("2 - Fire metodu ucun");
+                Console.WriteLine("3 - GetRemainBulletCount metodu ucun");
+                Console.WriteLine("4 - Reload metodu ucun");
+                Console.WriteLine("5 - ChangeFireMode metodu ucun");
+                Console.WriteLine("6 - Proqramdan cıxmaq ucun");
+
+                Console.Write("Secim edin: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine($"Tutum: {myWeapon.MagazineCapacity}, Cari gulle sayı: {myWeapon.CurrentBulletCount}, Mod: {myWeapon.FireMode}");
+                        break;
+                    case "1":
+                        myWeapon.Shoot();
+                        break;
+                    case "2":
+                        myWeapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine($"Tam dolması ucun lazım olan gulle sayı: {myWeapon.GetRemainBulletCount()}");
+                        break;
+                    case "4":
+                        myWeapon.Reload();
+                        break;
+                    case "5":
+                        myWeapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.WriteLine("Proqram bitdi.");
+                        return;
+                    default:
+                        Console.WriteLine("Yanlış secim! Yenidən cehd edin.");
+                        break;
+                }
+            }
+        }
+   }
+}
---
+++ Weapon/Weapon/Weapon.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon
+{
+   public class Weapon
+    {
+        
+    public int MagazineCapacity { get; private set; } 
+        public int CurrentBulletCount { get; private set; } 
+        public string FireMode { get; private set; } 
+
+        public Weapon(int magazineCapacity, int currentBulletCount, string fireMode)
+        {
+            MagazineCapacity = magazineCapacity;
+            CurrentBulletCount = currentBulletCount;
+            FireMode = fireMode;
+        }
+
+        public void Shoot()
+        {
+            if (CurrentBulletCount > 0)
+            {
+                CurrentBulletCount--;
+                Console.WriteLine("Bir gulle atıldı! Qalan gulle sayı: " + CurrentBulletCount);
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur! Yeniden doldurun.");
+            }
+        }
+
+        public void Fire()
+        {
+            if (CurrentBulletCount > 0)
+            {
+                Console.WriteLine("Avtomatik atıs basladı:");
+                while (CurrentBulletCount > 0)
+                {
+                    Shoot();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Daraqda gulle yoxdur!");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return MagazineCapacity - CurrentBulletCount;
+        }
+
+        public void Reload()
+        {
+            int bulletsToReload = GetRemainBulletCount();
+            CurrentBulletCount += bulletsToReload;
+            Console.WriteLine("Daraq tamamile dolduruldu! Cari gulle sayı: " + CurrentBulletCount);
+        }
+
+        public void ChangeFireMode()
+        {
+            FireMode = FireMode == "Single" ? "Auto" : "Single";
+            Console.WriteLine("Atıs modu deyisdirildi. Yeni mod: " + FireMode);
+        }
+    }
+
+}
+
